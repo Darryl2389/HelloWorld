228699cea744cfa34d6ed7af791e21fb66542c53
Darryl Sullivan <N00163239@iadt.ie>
2019-09-24 14:14:25 +0100

    MyFirstCommit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+# @Date:   2019-09-17T15:42:25+01:00
+# @Last modified time: 2019-09-24T13:59:42+01:00
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcomeDarryl');
+});
+
+Route::get('/HelloWorld', function () {
+    return "<p> Hello World </p>";
+});
